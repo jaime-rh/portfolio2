aa6b3044aaa49527e8ae3b48b9cfe4fb69769c14
jaime-rh <jaimeromerohernandez4@gmail.com>
2025-12-04 16:12:02 +0100

    rt5

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { IdiomaService } from '../services/idioma.service';
@@ -10,13 +10,14 @@ import { IdiomaService } from '../services/idioma.service';
   templateUrl: './cv.component.html',
   styleUrls: ['./cv.component.css']
 })
-export class CVComponent {
+export class CVComponent implements OnInit {
   idiomaActual: 'es' | 'en' = 'es';
 
   pdfFilePath: string = 'assets/pdf/cv-jaime-romero-hernandez.pdf';
-
-  // Variable segura para el object
   pdfFileSafe!: SafeResourceUrl;
+
+  pdfImages: string[] = [];
+  isMobile: boolean = false;
 
   textos: Record<'es' | 'en', { descargar: string }> = {
     es: { descargar: 'Descargar CV' },
@@ -26,22 +27,51 @@ export class CVComponent {
   constructor(
     private idiomaService: IdiomaService,
     private sanitizer: DomSanitizer
-  ) {
-    this.actualizarPDF(this.pdfFilePath);
+  ) {}
 
-    // Suscribirse a cambios de idioma
+  ngOnInit(): void {
+    // Solo ejecutamos window si existe (navegador)
+    if (typeof window !== 'undefined') {
+      this.detectDevice();
+    }
+
+    this.actualizarPDF(this.pdfFilePath);
+    this.actualizarImagenes();
+
     this.idiomaService.idioma$.subscribe((idioma) => {
       this.idiomaActual = idioma as 'es' | 'en';
+
       const ruta = this.idiomaActual === 'es'
         ? 'assets/pdf/cv-jaime-romero-hernandez.pdf'
         : 'assets/pdf/cv-jaime-romero-hernandez-en.pdf';
       this.actualizarPDF(ruta);
+
+      this.actualizarImagenes();
     });
   }
 
   actualizarPDF(ruta: string) {
     this.pdfFilePath = ruta;
-    // Marcar URL como segura
     this.pdfFileSafe = this.sanitizer.bypassSecurityTrustResourceUrl(ruta);
+  }
+
+  actualizarImagenes() {
+    if (this.isMobile) {
+      this.pdfImages = this.idiomaActual === 'es'
+        ? [
+            'assets/pdf/cv-esp-1.png',
+            'assets/pdf/cv-esp-2.png'
+          ]
+        : [
+            'assets/pdf/cv-en-1.png',
+            'assets/pdf/cv-en-2.png'
+          ];
+    } else {
+      this.pdfImages = [];
+    }
+  }
+
+  detectDevice() {
+    this.isMobile = window.innerWidth <= 1024;
   }
 }
